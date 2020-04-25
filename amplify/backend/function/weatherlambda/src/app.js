3a9f4bb6af80ea78e9e8a292ff2383be7f25095c
Stephen Rayner <stephenrrayner@dev.azure.com>
2020-04-25 14:00:24 +0100

    created lambda function for weather data

---
+++ amplify/backend/function/weatherlambda/src/app.js
@@ -0,0 +1,29 @@
+var express = require('express')
+var bodyParser = require('body-parser')
+var awsServerlessExpressMiddleware = require('aws-serverless-express/middleware')
+
+// declare a new express app
+var app = express()
+app.use(bodyParser.json())
+app.use(awsServerlessExpressMiddleware.eventContext())
+
+// Enable CORS for all methods
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+  next()
+});
+
+app.get('/items', function(req, res) {
+  const weather = [{ temperature: 23, location: 'Oxford' }];
+  res.json({success: 'get call succeed!', url: req.url, weather});
+});
+
+app.listen(3000, function() {
+    console.log("App started")
+});
+
+// Export the app object. When executing the application local this does nothing. However,
+// to port it to AWS Lambda we will create a wrapper around that will load the app from
+// this file
+module.exports = app
