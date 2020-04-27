3ad2b7b351c7b37f56274a6011191519a57233c3
Stephen Rayner <stephenrrayner@dev.azure.com>
2020-04-27 21:19:03 +0100

    Updates

---
+++ precache-manifest.2b4d465a4f21dbca9e518924a7d6412c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "218c4e03fcacba515197bec93449753c",
+    "url": "/squareduptest/index.html"
+  },
+  {
+    "revision": "2ed4fbdd548e29ed923f",
+    "url": "/squareduptest/static/css/main.52d43232.chunk.css"
+  },
+  {
+    "revision": "8335d4a02442ededeef8",
+    "url": "/squareduptest/static/js/2.662b4388.chunk.js"
+  },
+  {
+    "revision": "625355676f5fd1abc357ab49d8f09582",
+    "url": "/squareduptest/static/js/2.662b4388.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ed4fbdd548e29ed923f",
+    "url": "/squareduptest/static/js/main.7c477e3c.chunk.js"
+  },
+  {
+    "revision": "2721aa11f10ff3f20ecb",
+    "url": "/squareduptest/static/js/runtime-main.7aba2ee7.js"
+  },
+  {
+    "revision": "c57a9a0561b160c6ff698806ad75f15b",
+    "url": "/squareduptest/static/media/hc.c57a9a05.png"
+  }
+]);
