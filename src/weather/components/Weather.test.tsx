76c9a7bf277f9f0e5dd910680390308e83e8479b
Stephen Rayner <stephenrrayner@dev.azure.com>
2020-04-27 21:12:41 +0100

    Created tests for the Weather component

--- src/weather/components/Weather.test.tsx
+++ src/weather/components/Weather.test.tsx
@@ -1,5 +1,28 @@
 import React from "react";
+import ReactDOM from "react-dom";
+import { shallow, mount } from "enzyme";
+import { Weather, WeatherIllistration, WeatherContainer, WeatherLocations, WeatherInfo } from ".";
+import { FabClose } from "../../fabButton/components/FabClose";
+import { Progress } from "../../progress";
 
 describe("Weather component", () => {
+    it("contains FabClose component", () => {
+        const wrapper = shallow(<Weather />);
+        expect(wrapper.find(FabClose)).toHaveLength(1);
+    });
 
+    it("contains WeatherIllistration component", () => {
+        const wrapper = shallow(<Weather />);
+        expect(wrapper.find(WeatherIllistration)).toHaveLength(1);
+    });
+
+    it("contains WeatherContainer component", () => {
+        const wrapper = shallow(<Weather />);
+        expect(wrapper.find(WeatherContainer)).toHaveLength(1);
+    });
+
+    it("contains WeatherLocations component", () => {
+        const wrapper = shallow(<Weather />);
+        expect(wrapper.find(WeatherLocations)).toHaveLength(1);
+    });
 });
